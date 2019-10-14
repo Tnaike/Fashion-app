09669393edd1971ea35782474af36bfbcd9d9e67
Tbaze <timmynaike@gmail.com>
2019-10-14 06:56:03 +0100

    added 404 page

---
+++ 404.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge, chrome=1" />
+    <meta name="format-detection" content="telephone=no" />
+    <title>Page not found - clientbaze</title>
+    <link rel="shortcut icon" href="images/icons/measuring-tape.png" />
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=satisfy" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="font/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" media="all" href="css/style.css" />
+</head>
+
+<body>
+    <div class="page-wrapper">
+        <div class="error-layout">
+            <div class="error-code m-bt-10">404 <i class="fa fa-warning"></i></div>
+            <div class="error-content">
+                <div class="error-message">We couldn't find it...</div>
+                <div class="error-desc m-bt-20">
+                    The page you're looking for doesn't exist.
+                </div>
+                <div>
+                    <a href="login.php" class="btn btn-primary txt-white">Go Back to Home Page</a>
+                </div>
+            </div>
+        </div>
+</body>
+</html>
